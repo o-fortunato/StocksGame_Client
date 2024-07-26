fb5dda5328789beea9e9c1738c39436745fc8932
o-fortunato <dani.04@outlook.pt>
2024-07-26 15:38:17 +0100

    Feature: Added DTO Templates

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,11 +1,50 @@
-
+/*import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {config} from "chai";*/
+const WebSocket = require('ws');
 
 const webSocket = new WebSocket('ws://summercamp24.ddns.net:4000');
 
+//DTO Templates (Id currently NOT automatic -- needs change)
+
+let TransactionDTO = {
+    'playerId': "db3bb7de-ad8b-47f1-8ae4-7e7c09a16285", //For now, change as needed
+    'stockId': "b04dd987-e5aa-48bd-88e4-ed4b36b8e823",
+    'quantity': 10
+}
+
+let PlayerDTO = {
+    "id": "a6cd5f0d-a0bc-4ad2-82ad-1133b9ea7a44", //For now, change as needed
+    "name": "Orlando",
+    "netWorth": 10000,
+    "wallet": [],
+    "chatMessages": []
+}
+let StockDTO = {
+    "id": "",
+    "company": "",
+    "description": "",
+    "symbol": "",
+    "price": "",
+}
+
+let ChatMessageDTO = {
+    "senderId": "a6cd5f0d-a0bc-4ad2-82ad-1133b9ea7a44", //For now, change as needed
+    "receiverId": "all",
+    "message": "Mekie maltinha"
+}
+//Use Test_Send to test data sent through Web Socket
+
+let Test_Send = JSON.stringify({
+    //See PDF for list of events
+    "event": "connect-confirm",
+    "data": PlayerDTO,
+})
+
 webSocket.addEventListener('open', (event) => {
     console.log('[WebSocket] Connection established');
+    webSocket.send(Test_Send);
 });
-
 
 
 webSocket.addEventListener('message', (event) => {
